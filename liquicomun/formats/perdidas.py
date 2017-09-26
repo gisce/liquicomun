fb9a71943e1d4e270e72b6627073d7b3b2ffce3b
Xavi Torelló <info@xaviertorello.cat>
2017-09-26 19:12:17 +0200

    Add a main "intelligent" Perdidas class
    Initializes the expected class based on the filename

--- liquicomun/formats/perdidas.py
+++ liquicomun/formats/perdidas.py
@@ -1,5 +1,20 @@
 from .ree import REEformat
 
+available_tariffs = {
+    '2.0A': Perd20A,
+    '2.0DHA': Perd20DH,
+    '2.0DHS': Perd20DHS,
+    '2.1A': Perd21A,
+    '2.1DHA': Perd21DH,
+    '2.1DHS': Perd21DHS,
+    '3.0A': Perd31A,
+    '3.1A': Perd21A,
+    '6.1': Perd61,
+    '6.1A': Perd61A,
+    '6.1B': Perd61B,
+}
+
+
 class Perd20A(REEformat):
     ''' PERD20A from XN_liquicom_YYYYMM.zip esios file'''
     name = 'perd20A'
@@ -94,3 +109,16 @@ class Perd61B(REEformat):
 
     def __init__(self, file=None, token=None):
         super(Perd61B, self).__init__(file=file, token=token)
+
+
+class Perdidas(REEformat):
+    ''' A generic Perdidas class '''
+    def __init__(self, file=None, token=None):
+        filename = file.split("_")
+        assert len(filename) > 1 and filename[1], "Filename '{}' is not valid".format(file)
+        tariff = filename[1]
+
+        # Return the class depending on the filename or raise an error if not matched
+        assert tariff in available_tariffs, "Losses for tariff '{}' not defined as available tariff type ('{}')".format(tariff, available_tariffs)
+        loss_tariff = available_tariffs[tariff]
+        return loss_tariff(file=file, token=token)
