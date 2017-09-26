6372280cc0e005a3fe504f84564958e9fba2094c
Xavi Torelló <info@xaviertorello.cat>
2017-09-26 20:18:08 +0200

    Simplify init definition

--- liquicomun/formats/perdidas.py
+++ liquicomun/formats/perdidas.py
@@ -98,6 +98,21 @@ class Perd61B(REEformat):
     def __init__(self, file=None, token=None):
         super(Perd61B, self).__init__(file=file, token=token)
 
+
+tariffs_to_losses = {
+    '2.0A': 'perd20A',
+    '2.0DHA': 'perd20D',
+    '2.0DHS': 'perd20DHS',
+    '2.1A': 'perd21A',
+    '2.1DHA': 'perd21D',
+    '2.1DHS': 'perd21DHS',
+    '3.0A': 'perd30A',
+    '3.1A': 'perd31A',
+    '6.1': 'perdg61',
+    '6.1A': 'perdg61A',
+    '6.1B': 'perdg61B',
+}
+
 available_perd_tariffs = {
     'perd20A': Perd20A,
     'perd20D': Perd20DH,
