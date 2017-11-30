6eb3fbf546b77d9a6f2a51dff39d5cf319bcace9
Xavi Torelló <info@xaviertorello.cat>
2017-11-30 20:38:02 +0100

    Use cache if exists for the test

--- specs/perdidas_spec.py
+++ specs/perdidas_spec.py
@@ -11,6 +11,16 @@ spec_VCR = vcr.VCR(
     cassette_library_dir=fixtures_path
 )
 
+called = {
+    "by_dict": {
+        'tariff': '2.0A',
+        'date_start': '20171001',
+        'date_end': '20171031',
+    },
+    "by_filename": "A1_perd20A_20171001_20171031"
+}
+
+
 with description('A new'):
     with before.each:
         pass
@@ -18,11 +28,6 @@ with description('A new'):
         with context('of losses'):
             with it('must be performed as expected'):
                 with spec_VCR.use_cassette('losses.yaml'):
-                    formats.REEformat.clear_cache()
-                    called = {
-                        'tariff': '2.0A',
-                        'date_start': '20171001',
-                        'date_end': '20171031',
-                    }
-                    loss = Perdida(**called)
+                    #formats.REEformat.clear_cache()
+                    loss = Perdida(**called['by_dict'])
                     print (loss.matrix)
