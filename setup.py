2b6a9789490191f2bcb795462d4e3933cdb17561
Eduard Carreras <ecarreras@gisce.net>
2019-09-06 12:17:53 +0200

    Bump to v0.2.0

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='liquicomun',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     url='https://github.com/gisce/liquicomun',
     license='MIT',
-    install_requires=['esios'],
+    install_requires=[
+        'esios',
+        'pytz',
+        'future',
+    ],
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
     description='Interact with REE liquicomun files',
